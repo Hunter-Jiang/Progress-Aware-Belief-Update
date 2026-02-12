3301d0242fac2b77ecef26e15e426a0a3aedc008
Hunter <156235109+Hunter-Jiang@users.noreply.github.com>
2026-02-12 16:42:46 -0500

    Update setup.sh
    setup update

--- agentenvs/agentenv-tool/setup.sh
+++ agentenvs/agentenv-tool/setup.sh
@@ -14,6 +14,6 @@ pip install -e .
 
 current_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 export PROJECT_PATH="$current_dir/Toolusage"
-export MOVIE_KEY="eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlZTBmYTQ1YjIxZmQ1Y2ZhMzI1Nzk2YmVmNTJiNDM0ZSIsIm5iZiI6MTc2NTkxODkyOS4xMDksInN1YiI6IjY5NDFjOGQxMmFiNDc5NmQ4MjNmODMzZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.yH8EARPE4yXUV_GWS0OOL1nVbjSK9uuI0cyoYb9oyU8"
-export TODO_KEY="39f9ccd0a2d587ae16ee44d747b4e3b784e156ae"
-export SHEET_EMAIL="hjiang24@ncu.edu"
+export MOVIE_KEY=""
+export TODO_KEY=""
+export SHEET_EMAIL=""
